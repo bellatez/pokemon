f8238b0e520d17cd8f049321bcdfa8868101fa49
TG <gisele.takwi@gmail.com>
2020-09-23 19:10:23 +0100

    changes

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container d-flex h-100 mt-5">
+    <div class="container mt-5">
         @if(session()->has('error'))
             <div class="alert alert-danger">
                 {{ session()->get('error') }}
@@ -12,13 +12,13 @@
                 {{ session()->get('success') }}
             </div>
         @endif
-        <div class="row row h-100 justify-content-center align-items-center">
+        <div class="row">
             <div class="col-12">
-            <form class="form-inline" method="get" action="{{Route('search.results')}}">
+                <form class="form-inline" method="get" action="{{Route('search.results')}}">
                     <div class="form-group mb-2">
-                      <label for="" class="sr-only">Search By:</label>
+                      <label for=""><b>Search By:</b>  &nbsp;</label>
                       <div class="form-group mb-0">
-                        <select class="form-control form-search-control bg-white border-0" name="option" id="exampleFormControlSelect1">
+                        <select class="form-control form-search-control" name="option" id="exampleFormControlSelect1">
                             <option value="name" {{app('request')->input('option') == 'name' ? 'selected': ''}}>name</option>
                             <option value="rarity" {{app('request')->input('option') == 'rarity' ? 'selected': ''}}>Rarity</option>
                             <option value="hp" {{app('request')->input('option') == 'hp' ? 'selected': ''}}>Hip points</option>
@@ -31,14 +31,19 @@
                     <button type="submit" class="btn btn-primary mb-2">Search</button>
                 </form>
             </div>
+        </div>
+        <h2 class="text-center mt-4">Search Results</h2>
+        <div class="row text-center">
             @isset($results)
-            @if (count($results) > 0)
-                @foreach($results as $card)
-                    <div class="col">
-                        <img src="{{$card['imageUrl']}}" alt="">
-                    </div>
-                @endforeach
-            @endif
+                @if (count($results) > 0)
+                    @foreach($results as $card)
+                        <div class="col mt-4">
+                            <img src="{{$card['imageUrl']}}" alt="">
+                        </div>
+                    @endforeach
+                @else
+                    <h3>No data found for: <b>"{{ app('request')->input('query')}}"</b></h3>
+                @endif
             @endisset
         </div>
     </div>
