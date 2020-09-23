2ca1810f05b70b97c3d8c36f8978c41f63390a90
TG <gisele.takwi@gmail.com>
2020-09-23 18:07:09 +0100

    changes

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -22,28 +22,20 @@ class PokemonController extends Controller
     public function searchData(Request $request)
     {
         //convert json data to collection inorder to perform queries
-        if($this->dataset == null){
-            
-        }
-        $collection = collect($this->dataset['cards']);
-
         $q = $request->get('query');
         $search_by = $request->get('option');
         $results = [];
 
-        if($q == null){
-            Storage::put('backup.json', file_get_contents('https://api.pokemontcg.io/v1/cards?cards?setCode=base4'));
-            if ($search_by == 'name') {
-                $results = $collection->where('name', $q)->all();
-            }
-            if ($search_by == 'rarity') {
-                $results = $collection->where('rarity', $q)->all();
-            }
-            if ($search_by == 'hp') {
-                $results = $collection->where('hp', $q)->all();
-            }
 
-            return view('search', compact('results'));
+        if($this->dataset == null){
+            Storage::put('backup.json', file_get_contents('https://api.pokemontcg.io/v1/cards?cards?setCode=base4'));
+            $this->dataset = json_decode(Storage::disk('local')->get('backup.json'), true);
+
+        }
+        $collection = collect($this->dataset['cards']);
+
+         if ($q == null) {
+            return view('search');
 
         }
 
